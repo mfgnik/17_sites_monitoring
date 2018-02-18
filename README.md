24058563bc16dafe0eec70a6a0fb764f7f8ab0dc
Виктор Андрюшенков <buktop@MacBook-Air-Viktor.local>
2018-02-18 19:53:00 +0900

    First realease

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The script checks the status of sites on the following criteria:
 
+* the server responds to the request with the status of HTTP 200;
+* the domain name of the site is paid for at least 1 month in advance.
+
+At the input of the script comes a file with a list of url for verification.
+Recording format for the URL list:
+```
+google.com
+wikipedia.org
+devman.org
+pythonforbeginners.com
+
+```
+# Get started:
+
+An example of running a script in Linux, Python 3.5:
+
+```bash
+$ python3 check_sites_health.py -f domain_list
+google.com :
+				server respond with status 200 - OK
+				the domain name of the site is paid for 938 days
+wikipedia.org :
+				server respond with status 200 - OK
+				the domain name of the site is paid for 1789 days
+devman.org :
+				server respond with status 200 - OK
+				the domain name of the site is paid for 190 days
+
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,65 @@
-def load_urls4check(path):
-    pass
+import requests
+import whois
+import datetime
+import argparse
+
+
+def get_argv():
+    parser = argparse.ArgumentParser(
+        description='Utility for checking the status of sites'
+    )
+    parser.add_argument(
+        '--file',
+        '-f',
+        default='domain_list',
+        help='path to the file containing url sites',
+    )
+    return parser.parse_args()
+
+
+def load_urls4check(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read().split('\n')
+    except FileNotFoundError:
+        return None
+
 
 def is_server_respond_with_200(url):
-    pass
+    if requests.get('http://www.{}'.format(url)).ok:
+        return '200 ОК'
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    data_now = datetime.datetime.now()
+    try:
+        domain_inf = whois.whois(domain_name)
+        expiration_date = domain_inf.expiration_date
+        if type(domain_inf.expiration_date) is list:
+            expiration_date = domain_inf.expiration_date[0]
+        elif domain_inf.expiration_date is None:
+            return 'No data'
+        expiration_numbers_days = (expiration_date - data_now).days
+        return expiration_numbers_days
+    except whois.parser.PywhoisError:
+        return 'No data'
+
+
+def print_domain_inf(url):
+    print(url, ':')
+    server_respond_with_200 = 'OK'
+    if is_server_respond_with_200(url) is None:
+        server_respond_with_200 = 'Fall'
+    print('\t\t\t\tserver respond with status 200 - {}'.format(server_respond_with_200))
+    domain_expiration_date = get_domain_expiration_date(url)
+    print('\t\t\t\tthe domain name of the site is paid for {} days'.format(domain_expiration_date))
+
 
 if __name__ == '__main__':
-    pass
+    path_file = get_argv()
+    domain_list = load_urls4check(path_file.file)
+    if domain_list:
+        for domain_url in domain_list:
+            print_domain_inf(domain_url)
+    else:
+        print('File not found')
