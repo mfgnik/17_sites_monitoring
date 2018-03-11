a88f95aa645ff9c27e5d316c4852cd10d1494f1c
B1kt0p <a.biktop@gmail.com>
2018-03-11 21:38:31 +0900

    Fixed name value and function get_args, load_domain4check, domain and etc.

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,7 +4,7 @@ import datetime
 import argparse
 
 
-def get_argv():
+def get_args():
     parser = argparse.ArgumentParser(
         description='Utility for checking the status of sites'
     )
@@ -12,7 +12,7 @@ def get_argv():
         '--file',
         '-f',
         default='domain_list',
-        help='path to the file containing url sites',
+        help='path to the file containing domain sites',
     )
     parser.add_argument(
         '--paiddays',
@@ -24,7 +24,7 @@ def get_argv():
     return parser.parse_args()
 
 
-def load_urls4check(file_path):
+def load_domain4check(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             return file.read().split('\n')
@@ -32,38 +32,36 @@ def load_urls4check(file_path):
         return None
 
 
-def is_server_respond_with_ok(url):
+def is_server_respond_with_ok(domain):
     try:
-        return requests.get(url).ok
+        return requests.get(domain).ok
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
         return None
 
 
-def is_payment_expire(url, paid_days):
+def is_payment_expire(domain, paid_days):
     date_now = datetime.datetime.now()
     try:
-        domain_info = whois.whois(url)
+        domain_info = whois.whois(domain)
     except whois.parser.PywhoisError:
         return None
     expiration_date = domain_info.expiration_date
     if type(domain_info.expiration_date) is list:
         expiration_date = domain_info.expiration_date[0]
     elif domain_info.expiration_date is None:
-        return 'No data'
+        return None
     return (expiration_date - date_now).days > paid_days
+1
 
-
-def check_site_status(url, paid_days):
+def check_site_status(domain, paid_days):
     site_status = {}
-    if is_server_respond_with_ok(url):
+    if is_server_respond_with_ok(domain):
         site_status.update({'respond': 'OK'})
     else:
         site_status.update({'respond': 'NO CONNECT'})
-    payment_expire = is_payment_expire(url, paid_days)
-    if payment_expire == 'No data':
+    payment_expire = is_payment_expire(domain, paid_days)
+    if payment_expire is None:
         site_status.update({'payment': 'NO DATA'})
-    elif payment_expire is None:
-        site_status.update({'payment': 'NO CONNECT'})
     elif not payment_expire:
         site_status.update({'payment': 'NO PAY'})
     else:
@@ -72,18 +70,18 @@ def check_site_status(url, paid_days):
 
 
 if __name__ == '__main__':
-    argv = get_argv()
-    paid_days = get_argv().paiddays
-    domain_list = load_urls4check(argv.file)
+    args = get_args()
+    paid_days = get_args().paiddays
+    domain_list = load_domain4check(args.file)
     if domain_list:
-        print('\nCheck the status of the site from {}'.format(argv.file))
-        for url in domain_list:
-            site_status = check_site_status(url, paid_days)
-            print(url)
+        print('\nCheck the status of the site from {}'.format(args.file))
+        for domain in domain_list:
+            site_status = check_site_status(domain, paid_days)
+            print(domain)
             print ('Respond status - {}'.format(site_status['respond']))
             print('Paid period more than {} days : {}'.format(
                 paid_days, site_status['payment']
             ))
-            print('----------------------------------------------------------')
+            print('-'*80)
     else:
         print('File not found')
