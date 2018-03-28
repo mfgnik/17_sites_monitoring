b9cb806a6f7f5d44a5b09a696775e261bf4d7593
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-28 16:17:45 +0300

    Update check_sites_health.py

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,6 +4,23 @@ import datetime
 from urllib.parse import urlparse
 import sys
 import os
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_path',
+        help='path to the file with urls',
+        required=True
+    )
+    parser.add_argument(
+        '--days',
+        type=int,
+        default=30,
+        help='amount of days to check'
+    )
+    return parser.parse_args()
 
 
 def load_urls4check(path_to_file):
@@ -21,45 +38,40 @@ def is_server_respond_with_ok(url):
 
 
 def get_domain_expiration_date(domain_name):
-    return whois.whois(domain_name)['expiration_date']
+    expiration_date = whois.whois(domain_name)['expiration_date']
+    if type(expiration_date) == list:
+        return expiration_date[0]
+    return expiration_date
 
 
 def is_enough_days_remained(expiration_date, days=30):
+    if expiration_date is None:
+        return None
     delta = expiration_date - datetime.datetime.now()
     return delta.days > days
 
 
-def get_info_about_http_status(url):
+def get_info_about_site(url, days):
     try:
         print('Server name: {}'.format(url))
         if is_server_respond_with_ok(url):
-            return 'Server responds'
+            is_server_responds = 'Server responds with ok'
         else:
-            return 'Server does not respond with status HTTP 200'
+            is_server_responds = 'Server does not respond with ok'
     except requests.RequestException as e:
-        return 'Exception with HTTP status is: {}'.format(e)
-
-
-def get_info_about_expiration_date(url, days=30):
-    try:
-        expiration_date = get_domain_expiration_date(get_domain_name(url))
-        if is_enough_days_remained(expiration_date, days):
-            return 'Server does not expire in {} days'.format(days)
-        else:
-            return 'Server expires in {} days'.format(days)
-    except TypeError:
-        return 'Can not get expiration date'
+        is_server_responds = 'Exception with HTTP status is: {}'.format(e)
+    expiration_date = get_domain_expiration_date(get_domain_name(url))
+    if is_enough_days_remained(expiration_date, days):
+        date_info = 'Server does not expire in {} days'.format(days)
+    else:
+        date_info = 'Server expires in {} days'.format(days)
+    return is_server_responds, date_info
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1 or not os.path.isfile(sys.argv[1]):
+    arguments = parse_args()
+    if not os.path.isfile(arguments.input_path):
         sys.exit('Problems with file')
-    urls_file = sys.argv[1]
-    if len(sys.argv) > 2:
-        days = int(sys.argv[2])
-    else:
-        days = 30
-    for url in load_urls4check(urls_file):
+    for url in load_urls4check(arguments.input_path):
         print(50 * '-')
-        print(get_info_about_http_status(url))
-        print(get_info_about_expiration_date(url, days))
+        print(*get_info_about_site(url, arguments.days), sep='\n')
